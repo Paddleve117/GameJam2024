dafe8f962b0fc24afc998a7e6c4570db73627dc1
Paddleve117 <123141321+Paddleve117@users.noreply.github.com>
2024-07-06 00:02:07 +0800

    114514

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,9 +6,11 @@ public class Block : MonoBehaviour
 {
     // Start is called before the first frame update
     public float weight = 1;
+
     void Start()
     {
 
+        //114514
     }
 
     // Update is called once per frame
